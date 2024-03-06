cbfb8b3d059a592102e229dd5968ce4436870d4a
Georg Oli <162416758+edansnhicel@users.noreply.github.com>
2024-03-06 08:53:58 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+(function (root, factory) {
+	"use strict";
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery', 'tabulator', 'jquery-ui'], factory);
+	}
+	else if(typeof module !== 'undefined' && module.exports) {
+		module.exports = factory(
+			require('jquery'),
+			require('tabulator'),
+			require('jquery-ui')
+		);
+	}
+
+ else {
+		factory(root.jQuery, root.Tabulator);
+	}
+}(this, function ($, Tabulator) {
+
+	$.widget("ui.tabulator", {
+		_create:function(){
+			var options = Object.assign({}, this.options);
+			var props = [];
+
+			delete options.create;
+			delete options.disabled;
